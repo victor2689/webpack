7040cca866251c423b291a8cdff39cdaf2e99e72
victor2689 <victor.urbina.e@gmail.com>
2019-02-08 11:52:07 -0500

    Webpack

---
+++ src/app/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <img src="/dist/images/login-register.jpg" />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
